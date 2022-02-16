5cad2ccc043bfee8727c12b96aea9429bb62a61b
OromeSenryu <70779127+OromeSenryu@users.noreply.github.com>
2022-02-16 09:11:04 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import validator from './validator.js';
+      const validateCard = document.getElementById("validateCard")
+      const cardNumber = document.getElementById("inputCardNumber")
+       
+      
+      validateCard.addEventListener("click", function(){
+        const isValid = validator.isValid(cardNumber.value)
+        let maskNumber = validator.maskify(cardNumber.value)
+        let validateAlert = document.querySelector('#validateText');
+     
 
-console.log(validator);
+        if(isValid){
+          console.log("La tarjeta " + maskNumber + " es válida")
+          validateAlert.innerHTML = "La tarjeta " + maskNumber + " es válida"
+          validateAlert.classList.add("valid");
+        } else {
+          console.log("La tarjeta " + maskNumber + " es inválida")
+          validateAlert.innerHTML = "La tarjeta " + maskNumber + " es inválida"
+          validateAlert.classList.add("invalid");
+        }
+        
+      
+      })
